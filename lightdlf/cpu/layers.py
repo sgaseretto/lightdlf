46a2d022e440e18724ae454ab5d4ef8a333be5e3
Sebastian Gonzalez Aseretto <sgaseretto@gmail.com>
2019-07-18 19:57:57 -0400

    Se agregan las primeras capas, Lineal y Secuencial

---
+++ lightdlf/cpu/layers.py
@@ -0,0 +1,47 @@
+import numpy as np
+from lightdlf.cpu.core import Tensor
+
+
+class Layer(object):
+
+    def __init__(self):
+        self.parameters = list()
+
+    def get_parameters(self):
+        return self.parameters
+
+
+class Linear(Layer):
+
+    def __init__(self, n_inputs, n_outputs):
+        super().__init__()
+        W = np.random.randn(n_inputs, n_outputs) * np.sqrt(2.0 / (n_inputs))
+        self.weight = Tensor(W, autograd=True)
+        self.bias = Tensor(np.zeros(n_outputs), autograd=True)
+
+        self.parameters.append(self.weight)
+        self.parameters.append(self.bias)
+
+    def forward(self, input):
+        return Tensor.mm(input, self.weight) + self.bias.expand(0, len(input.data))
+
+
+class Sequential(Layer):
+
+    def __init__(self, layers=list()):
+        super().__init__()
+        self.layers = layers
+
+    def add(self, layer):
+        self.layers.append(layer)
+
+    def forward(self, input):
+        for layer in self.layers:
+            input = layer.forward(input)
+        return input
+
+    def get_parameters(self):
+        params = list()
+        for l in self.layers:
+            params += l.get_parameters()
+        return params
