ecc0b0ac84f7d87f38872e4e9ad3b7fdb71275f8
Sebastian Gonzalez Aseretto <sgaseretto@gmail.com>
2019-08-04 10:19:58 -0400

    Se agrega la capa recurrente para crear RNNs

--- lightdlf/cpu/layers.py
+++ lightdlf/cpu/layers.py
@@ -103,3 +103,38 @@ class CrossEntropyLoss(object):
 
     def forward(self, input, target):
         return input.cross_entropy(target)
+
+
+class RNNCell(Layer):
+    def __init__(self, n_inputs, n_hidden, n_output, activation='sigmoid'):
+        super().__init__()
+
+        self.n_inputs = n_inputs
+        self.n_hidden = n_hidden
+        self.n_output = n_output
+
+        if(activation == 'sigmoid'):
+            self.activation = Sigmoid()
+        elif(activation == 'tanh'):
+            self.activation = Tanh()
+        else:
+            raise Exception("Non-linearity not found")
+
+        self.w_ih = Linear(n_inputs=n_inputs, n_outputs=n_hidden)
+        self.w_hh = Linear(n_inputs=n_hidden, n_outputs=n_hidden)
+        self.w_ho = Linear(n_inputs=n_hidden, n_outputs=n_output)
+
+        self.parameters += self.w_ih.get_parameters()
+        self.parameters += self.w_hh.get_parameters()
+        self.parameters += self.w_ho.get_parameters()
+
+    def forward(self, input, hidden):
+        from_prev_hidden = self.w_hh.forward(hidden)
+        from_actual_input = self.w_ih.forward(input)
+        combined =  from_actual_input + from_prev_hidden
+        new_hidden = self.activation.forward(combined)
+        output = self.w_ho.forward(new_hidden)
+        return output, new_hidden
+
+    def init_hidden(self, batch_size=1):
+        return Tensor(np.zeros((batch_size, self.n_hidden)), autograd=True)
